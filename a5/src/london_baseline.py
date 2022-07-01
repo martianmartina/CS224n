7c3b424a6b4a011bf088f31cf26dca6abe0c9e76
martianmartina <zhuqy@shanghaitech.edu.cn>
2022-06-30 23:40:25 -0400

    clean n submit

---
+++ a5/src/london_baseline.py
@@ -0,0 +1,11 @@
+# Calculate the accuracy of a baseline that simply predicts "London" for every
+#   example in the dev set.
+# Hint: Make use of existing code.
+# Your solution here should only be a few lines.
+import utils
+from tqdm import tqdm
+predictions = []
+for line in tqdm(open('birth_dev.tsv')):
+    predictions.append('London')
+total, correct = utils.evaluate_places('birth_dev.tsv', predictions)
+print("Accuracy: ",100*correct/total,"% (",correct," out of ",total,")",sep='')
